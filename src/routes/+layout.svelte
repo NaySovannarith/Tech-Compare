cfeec65cb31a7f939bccc3926299dd3945ad01bf
NARA Phumin <nara.phumin@gmail.com>
2025-04-08 08:00:57 +0700

    root routing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,10 @@
     import "../app.css";
     import Header from "$lib/components/Header.svelte";
     import Footer from "$lib/components/Footer.svelte";
+    
   </script>
-  
+
+
   <Header />
   
   <main class="min-h-screen p-4 bg-gray-50">
