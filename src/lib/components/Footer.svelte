4e649f8abbc1cf0fe78b4dde4cd59a9ce5dc91b1
NARA Phumin <nara.phumin@gmail.com>
2025-04-12 02:13:05 +0700

    update

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,5 +1,4 @@
-<div class="bg-[#00332e]">
-<footer class=" w-full bg-[#00332e] text-white py-10 rounded-t-3xl">
+<footer class="bg-[#00332e] text-white px-8 py-10 rounded-t-3xl">
     <div class="max-w-7xl mx-auto grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-6">
       <!-- Brand -->
       <div>
@@ -71,5 +70,4 @@
       </p>
     </div>
   </footer>
-  </div>
   
