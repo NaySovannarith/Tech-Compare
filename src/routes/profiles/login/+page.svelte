dbd7dc85e834240e472f46d4e5e1038c6cb551a5
PhearakreachPR <164382011+PhearakreachPR@users.noreply.github.com>
2025-04-12 01:03:16 +0700

    Login

---
+++ src/routes/profiles/login/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import LoginModal from '$lib/components/LoginModal.svelte';
+    let showLogin = false;
+  </script>
+  
+  <button on:click={() => (showLogin = true)}>Open Login</button>
+  
+  <LoginModal bind:isOpen={showLogin} />
+  
